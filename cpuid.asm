ebf9874ad471edebfb8c1da0d9d7bb20822d586e
Lucas Souza <lucasgabriel3490@gmail.com>
2023-12-10 18:28:21 -0300

    cpuid updated

--- cpuid.asm
+++ cpuid.asm
@@ -1,39 +1,38 @@
 bits 64
 default rel
 
-section .data
+section .rodata
 
 msg: db 'Physical-address width supported by the processor -> '
-msglen: equ $-msg
+msglen equ $-msg
 msg2: db 'Logical-address width supported by the processor  -> '
-msg2len: equ $-msg2
+msg2len equ $-msg2
 
 section .text
 global _start
 _start:
-	push rbx
 	mov eax, 0x80000008
 	cpuid
 	lea rsi,[rsp - 4] 
 	push rsi
 	mov ebx,eax
-	mov r12, 0x1
+	xor ebp,ebp
 .printMsg1:
 	mov eax, 0x1
-	mov edi, 0x1
-	mov rsi, msg
+	mov edi, eax 
+	lea rsi, [msg]
 	mov edx, msglen
 	syscall
 	pop rsi
 .buffer:
 	movzx eax,bl
-	mov r11d, 0xa
-	mov byte [rsi], 0xa
+	mov ecx, 0xa
+	mov byte [rsi], cl
 	dec rsi
 .convertToAscii:
-	xor rdx,rdx
-	div r11
-	add rdx, 0x30
+	xor edx,edx
+	div rcx
+	add edx, 0x30
 	mov byte [rsi], dl
 	dec rsi
 	test eax,eax
@@ -41,28 +40,27 @@ _start:
 	inc rsi
 .printNumber:
 	mov eax, 0x1
-	mov edi, 0x1
+	mov edi, eax
 	lea rdx, [rsp - 3]
 	sub rdx, rsi
 	syscall
+	test ebp, ebp
+	jnz .exit
 	lea rsi, [rsp - 4]
-	cmp r12, 0x2
-	je .exit
 .printMsg2:
 	push rsi
 	mov eax, 0x1
-	mov edi, 0x1
-	mov rsi, msg2
+	mov edi, eax
+	lea rsi, [msg2]
 	mov edx, msg2len
 	syscall
 
 	pop rsi
 	mov bl,bh
-	inc r12
-	jmp .buffer
+	inc ebp
+	jmp _start.buffer
 .exit:
-	pop rbx
 	mov eax, 60
-	xor rdi, rdi
+	xor edi, edi
 	syscall	
 	
